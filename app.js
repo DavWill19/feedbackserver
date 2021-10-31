715ef71a41e54b2c5dc7d45067892cb4d580ae34
David Williams <davwill@live.com>
2021-10-31 15:13:44 -0400

    make it better

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(
   })
 );
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://wenvenmoralesurvey.netlify.app");
   res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
