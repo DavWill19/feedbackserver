3cdc586e38a5ed4cf4cb9a47dc8a99a44d0d300c
David Williams <davwill@live.com>
2021-10-31 14:20:00 -0400

    make it better

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
   cors({
-    origin: 'https://wenvenmoralesurvey.netlify.app/',
+    origin: 'http://localhost:3001',
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
   })
