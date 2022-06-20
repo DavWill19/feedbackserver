897fbdd92dbfa047109cf9efcf59f8e6cc1c43b1
David Williams <davwill@live.com>
2022-06-20 11:19:31 -0400

    uggh

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const crewUser = require('./models/crewuser');
+const User = require('./models/users');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
@@ -8,9 +8,9 @@ const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 const config = require('./config.js');
 
-exports.local = passport.use(new LocalStrategy(crewUser.authenticate()));
-passport.serializeUser(crewUser.serializeUser());
-passport.deserializeUser(crewUser.deserializeUser());
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey, {expiresIn: 3600});
